dbfdb3c87ca340d06cff1d8758f22333c0ca0908
sojack <sojack@gmail.com>
2021-04-24 10:25:35 -0400

    add top nav with targetf attr for links

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,6 +3,8 @@ import SEO from "../components/seo";
 import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
 import styled from "styled-components";
+import { Helmet } from "react-helmet";
+// import MenuItem from "../components/menuItem";
 
 const MenuContainer = styled.div`
   .menuSection {
@@ -254,27 +256,27 @@ const TopButton = styled.div`
   cursor: pointer;
 `;
 
-// const TomoMenu = styled.div`
-//   text-transform: uppercase;
-//   background: linear-gradient(
-//     to bottom,
-//     rgba(0, 0, 0, 0.2) 0%,
-//     rgba(0, 0, 0, 0) 100%
-//   );
-//   width: 100%;
-//   ul {
-//     list-style: none;
-//   }
-//   li {
-//     display: inline-block;
-//   }
-//   a {
-//     text-decoration: none;
-//     padding: 2rem 1rem;
-//     color: white;
-//     font-size: 1.2rem;
-//   }
-// `;
+const TomoMenu = styled.div`
+  text-transform: uppercase;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.2) 0%,
+    rgba(0, 0, 0, 0) 100%
+  );
+  width: 100%;
+  ul {
+    list-style: none;
+  }
+  li {
+    display: inline-block;
+  }
+  a {
+    text-decoration: none;
+    padding: 2rem 1rem;
+    color: white;
+    font-size: 1.2rem;
+  }
+`;
 
 const Menu = () => {
   const [menuState, setMenuState] = useState(false);
@@ -285,26 +287,38 @@ const Menu = () => {
 
   return (
     <Layout>
+      <Helmet>
+        <base target="_parent" />
+      </Helmet>
       <SEO title="menu" />
-      {/* <TomoMenu>
+      <TomoMenu>
         <ul>
           <li>
-            <a href="https://www.tomosushi.ca">home</a>
+            <a target="_parent" href="https://www.tomosushi.ca">
+              home
+            </a>
           </li>
           <li>menu</li>
           <li>
-            <a href="https://www.tomosushi.ca/gallery.html">gallery</a>
+            <a target="_parent" href="https://www.tomosushi.ca/gallery.html">
+              gallery
+            </a>
           </li>
           <li>
-            <a href="https://www.tomosushi.ca/contacts.html">contact</a>
+            <a target="_parent" href="https://www.tomosushi.ca/contacts.html">
+              contact
+            </a>
           </li>
           <li>
-            <a href="https://www.tomosushi.ca/richmondhill-online.html">
+            <a
+              target="_parent"
+              href="https://www.tomosushi.ca/richmondhill-online.html"
+            >
               order online
             </a>
           </li>
         </ul>
-      </TomoMenu> */}
+      </TomoMenu>
       <MenuNav id="top">
         <ul>
           <li>
